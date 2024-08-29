ea989fd92c8195dc5c21830b1eb54ab2e3f64449
qhilipp <philippkathoefer@icloud.com>
2024-08-29 11:46:57 +0200

    split models

--- include/View.h
+++ include/View.h
@@ -2,7 +2,7 @@
 
 #include <ncurses.h>
 #include <vector>
-#include <Objects.h>
+#include <Models.h>
 #include <string>
 
 using namespace std;
@@ -126,7 +126,6 @@ private:
 	void renderStats() {
 		renderStat(0, "Health", 0.3);
 		renderStat(1, "Speed", game->player.velocity.length() / game->player.terminalVelocity);
-		renderStat(2, "Moving dir", to_string(game->player.movingDirection.x));
 	}
 
 public:
